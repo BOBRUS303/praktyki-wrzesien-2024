11ad2314f90b1cc388c7d437c06499a1f29b0352
BOBRUS303 <a.kretosz@wp.pl>
2024-10-07 10:14:53 +0200

    .

---
+++ Praktyki2024.ScreenshotMaker/ImageRenderer.cs
@@ -0,0 +1,87 @@
+using PuppeteerSharp;
+
+namespace ScreenshotMaker;
+
+public class ImageRenderer : IDisposable
+{
+    private BrowserFetcher _browserFetcher;
+    private static readonly string[] options = new []{ "--window-size=3840,2160" };
+
+    public ImageRenderer()
+    {
+        _browserFetcher = new BrowserFetcher();
+    }
+
+    public async Task<byte[]> GetDataFromURL(string htmlUrl)
+    {
+        try
+        {
+            var client = new HttpClient();
+            var html = await client.GetStringAsync(htmlUrl);
+            
+            // DO WORK HERE()
+            // 1. Wyszukać w pobranym HTML wszystkie linki i wpisać do listy linkList
+
+            var linkList = new List<string>();
+            foreach (var link in linkList)
+            {
+                Console.WriteLine(link);
+            }
+            Console.WriteLine("=======================================================================");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        return null;
+    }
+
+    public void Dispose()
+    {
+        _browserFetcher = null;
+    }
+    
+    public async Task CreateScreen(string url)
+    {
+        // Download the Chromium browser if needed
+        await new BrowserFetcher().DownloadAsync();
+
+        // Launch the browser
+        using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+        {
+            Headless = true,
+            DefaultViewport = new ViewPortOptions()
+            {
+                Width = 2160,
+                Height = 3840,
+                IsLandscape = false,
+            },
+            Timeout = 3000,
+            Args = options,
+            AcceptInsecureCerts = true,
+            EnqueueAsyncMessages = true
+        });
+
+        // Create a new page
+        using var page = await browser.NewPageAsync();
+
+        // Navigate to the URL
+        var response = await page.GoToAsync(url);
+
+        // Set the viewport size if needed (optional)
+        await page.SetViewportAsync(new ViewPortOptions
+        {
+            Width = 2160,
+            Height = 3840
+        });
+
+        // Take the screenshot
+        await page.ScreenshotAsync(Path.Combine(Directory.GetCurrentDirectory(),"test_image_from_url_2.png"));
+
+        // Close the browser
+        await browser.CloseAsync();
+
+        Console.WriteLine("Screenshot saved!");
+    }
+}
